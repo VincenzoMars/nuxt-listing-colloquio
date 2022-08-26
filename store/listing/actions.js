c79ee133eb077e714f21769c01708f46f10dbc1e
MVRS <vincenzo.mars16@gmail.com>
2022-08-26 12:41:56 +0200

    feat(listing/actions): added firebase endpoints and formatted code

--- store/listing/actions.js
+++ store/listing/actions.js
@@ -1,37 +1,37 @@
 export default {
   async loadResources({ commit, dispatch }) {
-      const products = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/products`).then(res => res.json())
-      commit('setTotalProducts', products)
-      commit('setActiveProducts', products)
-      const filters = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/filters`).then(res => res.json())
-      commit('setTotalFilters', filters)
-      dispatch('createFilters')
-    },
-    createFilters({ commit, dispatch, getters }) {
-        getters.getTotalProducts.forEach(product => {
-            Object.entries(product.filters).forEach(productFilter => {
-              const filter = getters.getFilterByName(productFilter[0])
-              if (Array.isArray(productFilter[1])) {
-                // if a product value is an array and has more selections, then iterate each value selected and create values for filters
-                productFilter[1].forEach(productValue => {
-                    dispatch('createValue', { product, filter, productValue })
-                })
-              } else {
-                  const productValue = productFilter[1]
-                  dispatch('createValue', { product, filter, productValue })
-              }
-            })
-        })
-    },
-    createValue(_, { product, filter, productValue }) {
-        const value = filter.values.find(value => value.id === productValue)
-        if (isNaN(value.quantity)) {
-          value.quantity = 0
+    const products = await fetch(`https://nuxt-listing-default-rtdb.firebaseio.com/products.json`).then(res => res.json())
+    commit('setTotalProducts', products)
+    commit('setActiveProducts', products)
+    const filters = await fetch(`https://nuxt-listing-default-rtdb.firebaseio.com/filters.json`).then(res => res.json())
+    commit('setTotalFilters', filters)
+    dispatch('createFilters')
+  },
+  createFilters({ commit, dispatch, getters }) {
+    getters.getTotalProducts.forEach(product => {
+      Object.entries(product.filters).forEach(productFilter => {
+        const filter = getters.getFilterByName(productFilter[0])
+        if (Array.isArray(productFilter[1])) {
+          // if a product value is an array and has more selections, then iterate each value selected and create values for filters
+          productFilter[1].forEach(productValue => {
+              dispatch('createValue', { product, filter, productValue })
+          })
+        } else {
+            const productValue = productFilter[1]
+            dispatch('createValue', { product, filter, productValue })
         }
-        value.quantity++
-        if (!value.productIds) {
-          value.productIds = []
-        }
-        value.productIds.push(product.id)
-    },
+      })
+    })
+  },
+  createValue(_, { product, filter, productValue }) {
+    const value = filter.values.find(value => value.id === productValue)
+    if (isNaN(value.quantity)) {
+      value.quantity = 0
+    }
+    value.quantity++
+    if (!value.productIds) {
+      value.productIds = []
+    }
+    value.productIds.push(product.id)
+  },
 }
