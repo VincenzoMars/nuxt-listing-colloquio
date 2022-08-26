c2493ca74a0bfbe177d6f41e5578c6a6a09d2163
MVRS <vincenzo.mars16@gmail.com>
2022-08-26 11:23:08 +0200

    fix(listing/actions): moved logic for listing load into listing actions

--- store/index.js
+++ store/index.js
@@ -5,13 +5,8 @@ export default {
     listing
   },
   actions: {
-    async nuxtServerInit({ commit, dispatch }) {
-      const products = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/products`).then(res => res.json())
-      commit('listing/setTotalProducts', products)
-      commit('listing/setActiveProducts', products)
-      const filters = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/filters`).then(res => res.json())
-      commit('listing/setTotalFilters', filters)
-      dispatch('listing/createFilters')
-  },
+    async nuxtServerInit({ dispatch }) {
+      await dispatch('listing/loadResources')
+    },
   }
 }
--- store/modules/listing/actions.js
+++ store/modules/listing/actions.js
@@ -1,11 +1,18 @@
 export default {
+  async loadResources({ commit, dispatch }) {
+      const products = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/products`).then(res => res.json())
+      commit('setTotalProducts', products)
+      commit('setActiveProducts', products)
+      const filters = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/filters`).then(res => res.json())
+      commit('setTotalFilters', filters)
+      dispatch('createFilters')
+    },
     createFilters({ commit, dispatch, getters }) {
         getters.getTotalProducts.forEach(product => {
             Object.entries(product.filters).forEach(productFilter => {
-              // const filter =  getters['listing/getFilterByName'](productFilter[0])
               const filter = getters.getFilterByName(productFilter[0])
-              // if a product value has more selections, then iterate each value selected and create values for filters
               if (Array.isArray(productFilter[1])) {
+                // if a product value is an array and has more selections, then iterate each value selected and create values for filters
                 productFilter[1].forEach(productValue => {
                     dispatch('createValue', { product, filter, productValue })
                 })
