6f05ca97b87b3609b56cc6fa5476afac7e6b6a8a
MVRS <vincenzo.mars16@gmail.com>
2022-08-26 11:05:46 +0200

    feat(actions): added nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -3,5 +3,15 @@ import listing from './modules/listing/index.js'
 export default {
   modules: {
     listing
+  },
+  actions: {
+    async nuxtServerInit({ commit, dispatch }) {
+      const products = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/products`).then(res => res.json())
+      commit('listing/setTotalProducts', products)
+      commit('listing/setActiveProducts', products)
+      const filters = await fetch(`https://6308878b722029d9ddd1543b.mockapi.io/api/filters`).then(res => res.json())
+      commit('listing/setTotalFilters', filters)
+      dispatch('listing/createFilters')
+  },
   }
 }
